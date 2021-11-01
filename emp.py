f2584436fe4f124de710c0dd5a963396153496ee
justin-bi-princeton <justinbi@princeton.edu>
2021-11-01 19:56:32 -0400

    asdf

--- emp.py
+++ emp.py
@@ -2,7 +2,8 @@ from flask import Flask, request, make_response
 from flask import render_template
 # ----------------------------------------------------------------------
 
-app = Flask(__name__, template_folder='./templates', static_folder='./static')
+# app = Flask(__name__, template_folder='./templates', static_folder='./static')
+app = Flask(__name__)
 
 # ----------------------------------------------------------------------
 # Routes
