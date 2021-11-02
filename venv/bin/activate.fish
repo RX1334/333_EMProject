a0b9c135bd914457ae6521b5b683664053c14bca
justin-bi-princeton <justinbi@princeton.edu>
2021-11-01 20:05:53 -0400

    sdsaf

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/justin/Desktop/COS333/333_EMProject/Flask/venv"
+set -gx VIRTUAL_ENV "/Users/justin/Desktop/COS333/333_EMProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
