5fa95c5f0e8f993de7d35e9ac809a5f61c823b6e
Brian Tieu <btieu@princeton.edu>
2021-11-03 00:19:47 -0400

    test timeout of fumehood data

--- emp.py
+++ emp.py
@@ -12,6 +12,8 @@ app = Flask(__name__, template_folder='./templates', static_folder='./static')
 @app.route('/', methods=['GET'])
 def index():
     fumehoods_usage = get_fumehood_output()
-    html = render_template('index.html', fumehoods_usage=[fumehoods_usage[0][0], fumehoods_usage[1][0], fumehoods_usage[2][0], fumehoods_usage[3][0]])
+    if fumehoods_usage is None:
+        fumehoods_usage = ['OFF', 'OFF', 'OFF', 'OFF']
+    html = render_template('index.html', fumehoods_usage=fumehoods_usage)
     response = make_response(html)
     return response
