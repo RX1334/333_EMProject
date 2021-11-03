486c52371abc54bb33b6f601eebf9da9eebe19c3
justin-bi-princeton <justinbi@princeton.edu>
2021-11-03 16:15:14 -0400

    Fix double render bug while "updating" fume hood data
    More specifically, when the main index page gets the fumehood data,
    it now returns a dictionary with keys being an "id number" for
    a hood and vals being their current energy consumption. When
    index.html gets this response, it then uses jquery to find the
    relevant fumehood energy widget to update the text value.
    
    This fixes the double render since now it's not fetching the entire
    html page again and rerendering it inside the paragraph. This still
    fetches real time data, and better yet this method would only rerender
    the parts of the html page that need updating instead of the entirety
    of the page.
    
    The old code is still there commented out, if you want to compare to
    what was happening prior.

--- emp.py
+++ emp.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response
 from flask import render_template
 from database import get_fumehood_output
+import random
 # ----------------------------------------------------------------------
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
@@ -11,9 +12,20 @@ app = Flask(__name__, template_folder='./templates', static_folder='./static')
 # ----------------------------------------------------------------------
 @app.route('/', methods=['GET'])
 def index():
-    fumehoods_usage = get_fumehood_output()
-    if fumehoods_usage is None:
-        fumehoods_usage = ['OFF', 'OFF', 'OFF', 'OFF']
+    # fumehoods_usage = get_fumehood_output()
+    # if fumehoods_usage is None:
+    #     fumehoods_usage = ['OFF', 'OFF', 'OFF', 'OFF']
+    # key val pairs are fumehood_id and their energy use
+    fumehoods_usage = {}
+    for i in range(4):
+        fumehoods_usage[i] = random.randint(100, 200)
     html = render_template('index.html', fumehoods_usage=fumehoods_usage)
     response = make_response(html)
     return response
+
+@app.route('/fumehood_stuff', methods=['GET'])
+def fumehood_stuff():
+    fumehoods_usage = {}
+    for i in range(4):
+        fumehoods_usage[i] = random.randint(100, 200)
+    return fumehoods_usage
