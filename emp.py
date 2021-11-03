bc3e0ef11e13c0928fe4017318b86c99eef3fce9
Brian Tieu <btieu@princeton.edu>
2021-11-02 23:48:00 -0400

    fumehood output change

--- emp.py
+++ emp.py
@@ -12,6 +12,6 @@ app = Flask(__name__, template_folder='./templates', static_folder='./static')
 @app.route('/', methods=['GET'])
 def index():
     fumehoods_usage = get_fumehood_output()
-    html = render_template('index.html', fumehoods_usage=fumehoods_usage)
+    html = render_template('index.html', fumehoods_usage=[fumehoods_usage[0][0], fumehoods_usage[1][0], fumehoods_usage[2][0], fumehoods_usage[3][0]])
     response = make_response(html)
     return response
