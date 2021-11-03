bb52d7fe2e23102698ea117ae49faba0aaf3fd9b
Brian Tieu <btieu@princeton.edu>
2021-11-02 23:09:11 -0400

    playing with dark mode

--- emp.py
+++ emp.py
@@ -10,7 +10,7 @@ app = Flask(__name__, template_folder='./templates', static_folder='./static')
 # ----------------------------------------------------------------------
 @app.route('/', methods=['GET'])
 def index():
-    fumehoods_usage = ['150', '10', '15', 'OFF']
+    fumehoods_usage = ['150', '10', 'OFF', 'OFF']
     html = render_template('index.html', fumehoods_usage=fumehoods_usage)
     response = make_response(html)
     return response
