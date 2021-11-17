69a4583739401ed68e404163c03aa5f51ad7a792
Brian Tieu <btieu@princeton.edu>
2021-11-17 01:37:05 -0500

    margins

--- emp.py
+++ emp.py
@@ -71,10 +71,8 @@ def lab_summary():
 
     # include the fumehood widgets
     num_fumehoods = 4 # This should be data fetched from the database
-    fumehoods_usage = ['OFF', 123, 123, 123]
     dashboard_content += '<div class="fume-hood-widget-container widget-container">'
     for i in range(num_fumehoods):
-        fumehood_name = "Fume Hood #" + str(i + 1)
         fumehood_id = "fumehood" + str(i)
         dashboard_content += '<span>'
         dashboard_content += render_template('mini-fume-hood-widget.html', fumehood_id=fumehood_id)
@@ -107,7 +105,7 @@ def real_time_data():
     if lab_name:
         # Here, get the relevant data given a lab_name.
         # For now, we just use dummy data.
-        data_dict[lab_name + '-number'] = str(random.randint(0, 4)) +  ' of 4'
+        data_dict[lab_name + '-number'] = str(random.randint(0, 4)) +  ' of 4 Fume Hoods in Use'
         data_dict[lab_name + '-current-kw'] = str(round(random.uniform(0.5,1.5), 2)) + ' kW'
         data_dict[lab_name + '-today-kwh'] = str(round(random.uniform(2,4), 2)) + ' kWh'
         data_dict[lab_name + '-temperature'] = str(random.randint(70, 80)) + ' °F'
