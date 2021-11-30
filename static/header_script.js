9e6d2982cd94b75787576143ae37eec77ca4a0d4
Brian Tieu <btieu@princeton.edu>
2021-11-29 23:20:31 -0500

    rudimentary print

--- static/header_script.js
+++ static/header_script.js
@@ -16,16 +16,17 @@ function setLightMode() {
   $(".dark-icon").removeClass("invisible-icon");
 
   if (localStorage.getItem("menu_open") === "1")
-    $("#hamburger-icon").attr("src", "../static/images/X_dark.svg");
-  else $("#hamburger-icon").attr("src", "../static/images/List_dark.svg");
+  $(".hamburger-icon").addClass("invisible-icon")
+  else $(".x-icon").addClass("invisible-icon")
 
-  if (localStorage.getItem("money_mode_on") === "1")
-    $("#money-mode-icon").attr("src", "../static/images/MoneyMode_green.svg");
-  else { $("#money-mode-icon").attr("src", "../static/images/MoneyMode_dark.svg");
+  if (localStorage.getItem("money_mode_on") === "1") {
+    $(".money-mode-icon").addClass("invisible-icon")
+    $(".green-icon").removeClass("invisible-icon")
+  }
+  else $(".green-icon").addClass("invisible-icon")
 
   $(".report-widget-internal-container").addClass("light-report-widget-nest");
   $(".report-widget-internal-container").removeClass("report-widget-nest");
-}
 
   // change remaining css
   $("body, a").css({ color: "#2A2A2A" });
@@ -53,17 +54,17 @@ function setDarkMode() {
   $(".light-icon").removeClass("invisible-icon");
 
   if (localStorage.getItem("menu_open") === "1")
-    $("#hamburger-icon").attr("src", "../static/images/X.svg");
-  else $("#hamburger-icon").attr("src", "../static/images/List.svg");
+    $(".hamburger-icon").addClass("invisible-icon")
+  else $(".x-icon").addClass("invisible-icon")
 
-  if (localStorage.getItem("money_mode_on") === "1")
-    $("#money-mode-icon").attr("src", "../static/images/MoneyMode_green.svg")
-  else  { $("#money-mode-icon").attr("src", "../static/images/MoneyMode.svg")
+  if (localStorage.getItem("money_mode_on") === "1") {
+    $(".money-mode-icon").addClass("invisible-icon")
+    $(".green-icon").removeClass("invisible-icon")
+  }
+  else $(".green-icon").addClass("invisible-icon")
 
   $(".report-widget-internal-container").addClass("report-widget-nest");
   $(".report-widget-internal-container").removeClass("light-report-widget-nest");
-}
-
 
   // change remaining css
   $("body, a").css({ color: "white" });
@@ -91,9 +92,20 @@ function setMenuClosed() {
   $(".menu-hidden").hide();
   $(".menu-visible").show();
   // if dark mode is on, display white svg. else display gray one
-  if (localStorage.getItem("dark_mode_on") === "0")
-    $("#hamburger-icon").attr("src", "../static/images/List_dark.svg");
-  else $("#hamburger-icon").attr("src", "../static/images/List.svg");
+  if (localStorage.getItem("dark_mode_on") === "0") {
+    $(".dark-icon").removeClass("invisible-icon");
+    $(".x-icon").addClass("invisible-icon");
+    $(".light-icon").addClass("invisible-icon");
+  } else {
+    $(".light-icon").removeClass("invisible-icon");
+    $(".x-icon").addClass("invisible-icon");
+    $(".dark-icon").addClass("invisible-icon");
+  }
+
+  if (localStorage.getItem("money_mode_on") === "1") {
+    $(".dark-money-icon").addClass("invisible-icon");
+    $(".light-money-icon").addClass("invisible-icon");
+  }
 }
 
 function setMenuOpen() {
@@ -101,9 +113,20 @@ function setMenuOpen() {
   $(".menu-hidden").show();
   $(".menu-visible").hide();
   // if dark mode is on, display white svg. else display gray one
-  if (localStorage.getItem("dark_mode_on") === "0")
-    $("#hamburger-icon").attr("src", "../static/images/X_dark.svg");
-  else $("#hamburger-icon").attr("src", "../static/images/X.svg");
+  if (localStorage.getItem("dark_mode_on") === "0") {
+    $(".dark-icon").removeClass("invisible-icon");
+    $(".hamburger-icon").addClass("invisible-icon");
+    $(".light-icon").addClass("invisible-icon");
+  } else {
+    $(".light-icon").removeClass("invisible-icon");
+    $(".hamburger-icon").addClass("invisible-icon");
+    $(".dark-icon").addClass("invisible-icon");
+  }
+
+  if (localStorage.getItem("money_mode_on") === "1") {
+    $(".dark-money-icon").addClass("invisible-icon");
+    $(".light-money-icon").addClass("invisible-icon");
+  }
 }
 
 function toggleMenu() {
@@ -115,16 +138,18 @@ function setMoneyMode() {
   localStorage.setItem("money_mode_on", "1");
 
   // set icons to the correct color
-  $(".money-mode-icon").attr("src", "../static/images/MoneyMode_green.svg");
+  $(".money-mode-icon").addClass("invisible-icon");
+  $(".green-icon").removeClass("invisible-icon");
 }
 
 function setNonMoneyMode() {
   localStorage.setItem("money_mode_on", "0");
   // set icons to the correct color
+  $(".green-icon").addClass("invisible-icon");
   if (localStorage.getItem("dark_mode_on") === "0")
-    $(".money-mode-icon").attr("src", "../static/images/MoneyMode_dark.svg");
+    $(".dark-money-icon").removeClass("invisible-icon");
   else
-    $(".money-mode-icon").attr("src", "../static/images/MoneyMode.svg");
+    $(".light-money-icon").removeClass("invisible-icon");
 }
 
 function toggleMoneyMode() {
@@ -142,6 +167,7 @@ function toggleMoneyMode() {
 function setup() {
   $(".dark-mode-icon").on("click", toggleDarkMode);
   $(".hamburger-icon").on("click", toggleMenu);
+  $(".x-icon").on("click", toggleMenu);
   $(".money-mode-icon").on("click", toggleMoneyMode);
   console.log("setup");
 
