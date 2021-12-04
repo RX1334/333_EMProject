f4eb09fcf5d11e57677e1019c6857fc2adc30cd3
wolson1274 <70816273+wolson1274@users.noreply.github.com>
2021-12-04 13:56:58 -0500

    create

--- create_db.py
+++ create_db.py
@@ -14,21 +14,13 @@ from datetime import date, timedelta
 import mysql.connector
 from lab_query import lab_info
 
-def db_connect():
-    # Connect to database created with direct server connection
-    mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="wolson@Dev",
-    database ="energydb"
-    )
-    mycursor = mydb.cursor(buffered=True)
+def create_db(cursor):
     stmt_str = "CREATE TABLE curr_fhinfo("
     stmt_str += "fh_id VARCHAR(5), "
     stmt_str += "energy_consumption DECIMAL(10, 3), "
     stmt_str += "lab_id VARCHAR(10), "
     stmt_str += "PRIMARY KEY(fh_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE curr_labinfo("
     stmt_str += "lab_id VARCHAR(5), "
     stmt_str += "energy_consumption DECIMAL(10, 3), "
@@ -38,62 +30,72 @@ def db_connect():
     stmt_str += "energy_change VARCHAR(10), "
     stmt_str += "temperature VARCHAR(30), "
     stmt_str += "PRIMARY KEY(lab_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE day_fhinfo(" # reset every 24hr with cron
     stmt_str += "fh_id VARCHAR(5), "
     stmt_str += "lab_id VARCHAR(10), "
     stmt_str += "energy_consumption DECIMAL(10, 3), " #append to this with the curr data
     stmt_str += "PRIMARY KEY(fh_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE day_labinfo(" # reset every 24hr with cron
     stmt_str += "lab_id VARCHAR(5), "
     stmt_str += "fh_ids VARCHAR(30), " #string list of fh ids
     stmt_str += "energy_consumption DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(lab_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE week_fhinfo("
     stmt_str += "fh_id VARCHAR(5), "
     stmt_str += "lab_id VARCHAR(10), "
     stmt_str += "energy_consumption DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(fh_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE week_labinfo("
     stmt_str += "lab_id VARCHAR(5), "
     stmt_str += "fh_ids VARCHAR(30), " #string list of fh ids
-    stmt_str += "day VARCHAR(10), "
+    stmt_str += "day VARCHAR(10), " # dummy 0-6, map to real
     stmt_str += "energy_consumption DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(lab_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE month_fhinfo("
     stmt_str += "fh_id VARCHAR(5), "
-    stmt_str += "day VARCHAR(10), "
+    stmt_str += "day VARCHAR(10), " # dummy 0-6, map to real
     stmt_str += "energy_consumption DECIMAL(10, 3), " 
     stmt_str += "PRIMARY KEY(fh_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE month_labinfo("
     stmt_str += "lab_id VARCHAR(5), "
-    stmt_str += "fh_ids VARCHAR(30), " #string list of fh ids
-    stmt_str += "week VARCHAR(10), "
+    stmt_str += "week VARCHAR(10), " # dummy 0-4, map to real
     stmt_str += "total_consumption DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(lab_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE year_fhinfo("
     stmt_str += "fh_id VARCHAR(5), "
     stmt_str += "lab_id VARCHAR(10), "
-    stmt_str += "month VARCHAR(10), "
+    stmt_str += "month VARCHAR(10), " # dummy 0-11, map to real
     stmt_str += "energy_consumption DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(fh_id));"
-    mycursor.execute(stmt_str)
+    cursor.execute(stmt_str)
     stmt_str = "CREATE TABLE year_labinfo("
     stmt_str += "lab_id VARCHAR(5), "
-    stmt_str += "fh_ids VARCHAR(30), " #string list of fh ids
-    stmt_str += "month VARCHAR(10), "
+    stmt_str += "month VARCHAR(10), " # dummy 0-11, map to real
     stmt_str += "energy_consumption DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(lab_id));"
-    mydb.commit()
+    cursor.execute(stmt_str)
     # mycursor = mydb.cursor(buffered=True)
     # stmt_str = "SHOW TABLES"
     # print(mycursor.fetchall())
+
+def main():
+    # Connect to database created with direct server connection
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="wolson@Dev",
+    database ="energydb")
+    cursor = mydb.cursor(buffered=True)
+    create_db(cursor)
+    mydb.commit()
+    print('success')
 #---------------------------------------------------------
 if __name__ == '__main__':
-    db_connect()
+    main()
