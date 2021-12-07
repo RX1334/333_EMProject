5ea011b549cfcaa5dc2fd0cb6252100c119790b7
wolson1274 <70816273+wolson1274@users.noreply.github.com>
2021-12-06 19:00:29 -0500

    New structure

--- create_db.py
+++ create_db.py
@@ -23,110 +23,116 @@ def init_day_table(cursor):
     for lab in labs_fhs.keys():
         for fh in labs_fhs[lab]:
             fh_input = [str(fh),str(lab), 0.0, 0.0]
-            stmt_str = "INSERT INTO day_fhinfo(fh_id, lab_id, energy_consumption, hours_open) "
+            stmt_str = "INSERT INTO today_fhinfo(fh_id, lab_id, energy_consumption, hours_open) "
             stmt_str += "VALUES(%s, %s, %s, %s);"
             cursor.execute(stmt_str,fh_input)
         lab_input = [str(lab), 0.0, 0.0, 0.0]
-        stmt_str = "INSERT INTO day_labinfo(lab_id, fh_consumption, fh_opens, avg_consumption) "
+        stmt_str = "INSERT INTO today_labinfo(lab_id, fh_consumption, climate_consumption, total_consumption) "
         stmt_str += "VALUES(%s, %s, %s, %s);"
         cursor.execute(stmt_str,lab_input)
 
 def create_db(cursor):
-    stmt_str = "CREATE TABLE curr_fhinfo("
-    stmt_str += "fh_id VARCHAR(50), "
-    stmt_str += "is_open VARCHAR(5), "
-    stmt_str += "energy_consumption VARCHAR(1000), "
-    stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "PRIMARY KEY(fh_id));"
-    cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE curr_labinfo("
-    stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "fh_consumption DECIMAL(10, 3), "
-    stmt_str += "fh_opens VARCHAR(30), "
-    stmt_str += "consumption DECIMAL(10, 3), "
-    stmt_str += "PRIMARY KEY(lab_id));"
-    cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE day_fhinfo("
+    stmt_str = "CREATE TABLE today_fhinfo("
     stmt_str += "fh_id VARCHAR(50), "
     stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "energy_consumption DECIMAL(10, 3), " #append to this with the curr data
-    stmt_str += "hours_open INTEGER(5), " #append to this with the curr data
-    stmt_str += "PRIMARY KEY(fh_id));"
-    cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE day_labinfo(" 
-    stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "fh_consumption DECIMAL(10, 3), "
-    stmt_str += "fh_opens VARCHAR(30), "
-    stmt_str += "avg_consumption DECIMAL(10, 3), "
-    stmt_str += "PRIMARY KEY(lab_id));"
-    cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE week_fhinfo("
-    stmt_str += "id VARCHAR(50), "
-    stmt_str += "fh_id VARCHAR(50), "
-    stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "day VARCHAR(10), " # dummy 0-6, map to real
     stmt_str += "energy_consumption DECIMAL(10, 3), "
-    stmt_str += "hours_open INTEGER(2), "
+    stmt_str += "hours_open DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(fh_id));"
     cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE week_labinfo("
-    stmt_str += "id VARCHAR(50), "
+    stmt_str = "CREATE TABLE today_labinfo(" 
     stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "day VARCHAR(10), " # dummy 0-6, map to real
     stmt_str += "fh_consumption DECIMAL(10, 3), "
-    stmt_str += "fh_opens VARCHAR(30), "
-    stmt_str += "avg_consumption DECIMAL(10, 3), "
+    stmt_str += "climate_consumption DECIMAL(10,3), "
+    stmt_str += "total_consumption DECIMAL(10, 3), "
     stmt_str += "PRIMARY KEY(lab_id));"
     cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE month_fhinfo("
+    stmt_str = "CREATE TABLE daily_fhinfo("
     stmt_str += "id VARCHAR(50), "
     stmt_str += "fh_id VARCHAR(50), "
     stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "day VARCHAR(10), " # dummy 0-4, map to real
+    stmt_str += "day VARCHAR(50), " # dummy 0-5, map to real
+    stmt_str += "energy_consumption DECIMAL(10, 3), "
+    stmt_str += "hours_open DECIMAL(10,3), "
+    stmt_str += "PRIMARY KEY(id));"
+    cursor.execute(stmt_str)
+    stmt_str = "CREATE TABLE daily_labinfo("
+    stmt_str += "id VARCHAR(50), "
+    stmt_str += "lab_id VARCHAR(50), "
+    stmt_str += "day VARCHAR(50), " # dummy 0-5, map to real
+    stmt_str += "fh_consumption DECIMAL(10, 3), "
+    stmt_str += "climate_consumption DECIMAL(10,3), "
+    stmt_str += "total_consumption DECIMAL(10, 3), "
+    stmt_str += "PRIMARY KEY(id));"
+    cursor.execute(stmt_str)
+    stmt_str = "CREATE TABLE weekly_fhinfo("
+    stmt_str += "id VARCHAR(50), "
+    stmt_str += "fh_id VARCHAR(50), "
+    stmt_str += "lab_id VARCHAR(50), "
+    stmt_str += "week VARCHAR(50), " # dummy 0-3, map to real
+    stmt_str += "energy_consumption DECIMAL(10, 3), "
+    stmt_str += "hours_open DECIMAL(10,3), "
+    stmt_str += "PRIMARY KEY(id));"
+    cursor.execute(stmt_str)
+    stmt_str = "CREATE TABLE weekly_labinfo("
+    stmt_str += "id VARCHAR(50), "
+    stmt_str += "lab_id VARCHAR(50), "
+    stmt_str += "week VARCHAR(50), " # dummy 0-3, map to real
+    stmt_str += "fh_consumption DECIMAL(10, 3), "
+    stmt_str += "climate_consumption DECIMAL(10,3), "
+    stmt_str += "total_consumption DECIMAL(10, 3), "
+    stmt_str += "PRIMARY KEY(id));"
+    cursor.execute(stmt_str)
+    stmt_str = "CREATE TABLE monthly_fhinfo("
+    stmt_str += "id VARCHAR(50), "
+    stmt_str += "fh_id VARCHAR(50), "
+    stmt_str += "lab_id VARCHAR(50), "
+    stmt_str += "month VARCHAR(50), " # dummy 0-11, map to real
     stmt_str += "energy_consumption DECIMAL(10, 3), " 
-    stmt_str += "hours_open INTEGER(2), "
-    stmt_str += "PRIMARY KEY(fh_id));"
+    stmt_str += "hours_open DECIMAL(10, 3), "
+    stmt_str += "PRIMARY KEY(id));"
     cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE month_labinfo("
+    stmt_str = "CREATE TABLE monthly_labinfo("
     stmt_str += "id VARCHAR(50), "
     stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "week VARCHAR(10), " # dummy 0-4, map to real
+    stmt_str += "month VARCHAR(50), " # dummy 0-11, map to real
     stmt_str += "fh_consumption DECIMAL(10, 3), "
-    stmt_str += "fh_opens VARCHAR(30), "
-    stmt_str += "avg_consumption DECIMAL(10, 3), "
-    stmt_str += "PRIMARY KEY(lab_id));"
+    stmt_str += "climate_consumption DECIMAL(10,3), "
+    stmt_str += "total_consumption DECIMAL(10, 3), "
+    stmt_str += "PRIMARY KEY(id));"
     cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE year_fhinfo("
+    stmt_str = "CREATE TABLE yearly_fhinfo("
     stmt_str += "id VARCHAR(50), "
     stmt_str += "fh_id VARCHAR(50), "
     stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "month VARCHAR(10), " # dummy 0-11, map to real
+    stmt_str += "year VARCHAR(50), " # dummy 0-4, map to real
     stmt_str += "energy_consumption DECIMAL(10, 3), "
-    stmt_str += "hours_open INTEGER(2), "
-    stmt_str += "PRIMARY KEY(fh_id));"
+    stmt_str += "hours_open DECIMAL(10, 3), "
+    stmt_str += "PRIMARY KEY(id));"
     cursor.execute(stmt_str)
-    stmt_str = "CREATE TABLE year_labinfo("
+    stmt_str = "CREATE TABLE yearly_labinfo("
     stmt_str += "id VARCHAR(50), "
     stmt_str += "lab_id VARCHAR(50), "
-    stmt_str += "month VARCHAR(10), " # dummy 0-11, map to real
+    stmt_str += "year VARCHAR(50), " # dummy 0-4, map to real
     stmt_str += "fh_consumption DECIMAL(10, 3), "
-    stmt_str += "fh_opens VARCHAR(30), "
-    stmt_str += "avg_consumption DECIMAL(10, 3), "
-    stmt_str += "PRIMARY KEY(lab_id));"
+    stmt_str += "climate_consumption DECIMAL(10,3), "
+    stmt_str += "total_consumption DECIMAL(10, 3), "
+    stmt_str += "PRIMARY KEY(id));"
     cursor.execute(stmt_str)
     init_day_table(cursor)
 
 def main():
+    try:
     # Connect to database created with direct server connection
-    mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="wolson@Dev",
-    database ="energydb")
-    cursor = mydb.cursor(buffered=True)
-    create_db(cursor)
-    mydb.commit()
-    print('success')
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="wolson@Dev",
+        database ="energydb")
+        cursor = mydb.cursor(buffered=True)
+        create_db(cursor)
+        mydb.commit()
+    except Exception as ex:
+        print(ex, stderr)
 #---------------------------------------------------------
 if __name__ == '__main__':
     main()
