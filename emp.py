32d276b57df62bea5b0396d764745aafbb23cb87
Brian Tieu <btieu@princeton.edu>
2021-12-07 16:48:45 -0500

    lab dash is nicer

--- emp.py
+++ emp.py
@@ -110,8 +110,10 @@ def lab_summaries():
     # compiles widgets
     dashboard_content = render_template('header-widget.html', page_name='Lab Dashboard')
     dashboard_content += render_template('heading-label.html', text='Your Monitored Rooms')
+    dashboard_content += '<div class="lab-summary-widget-container widget-container">'
     dashboard_content += render_template('lab-summary-widget.html', lab_id='rabinowitz_icahn_201', lab_name='Rabinowitz, Icahn 201')
     dashboard_content += render_template('lab-summary-widget.html', lab_id='rabinowitz_icahn_202', lab_name='Rabinowitz, Icahn 202')
+    dashboard_content += '</div>'
 
     # renders dashboard with those widgets
     html = render_template('master_template.html', dashboard_content=dashboard_content)
